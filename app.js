46a60632266b0d4bc2f0563198fabe256f632b7d
truptihirani <hiranitrupti99@gmail.com>
2024-01-23 11:30:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,175 @@
+//array
+/*let fruits= ['mango','orange','apple','berry','banana']
+//array destructuring(es6)
+console.log (fruits[1])*/
+
+//objects
+/*let hirani={
+    fullname:'Hirani Trupti',
+    age:18,
+    color: 'Brown',
+    //function inside an object as a property is called a method
+    hello:function(){
+        console.log("hello")
+    }
+}
+console.log (hirani.hello())*/
+
+//functions
+/*function fullname(){
+    console.log (" my fullname is Trupti Hirani")
+}s
+fullname()*/
+
+/*let fname="Trupti"
+let sname="Mahesh"
+let lname="Hirani"
+let age=18
+
+console.log(`my name is ${fname} second name is ${sname} last name is ${lname} and my age is ${age}`)*/
+
+/*function fullname(firstname,lastname,age,education){
+
+    console.log(`my name is ${firstname} ${lastname}  my age is ${age} and my education is ${education}`)
+}
+fullname("trupti","hirani",18,"highschool")*/
+
+/*function sum(firstnumber,secondnumber,thirdnumber){
+
+    console.log(`sum of ${firstnumber} + ${secondnumber} + ${thirdnumber}`)
+}
+sum(18,21,32)*/
+
+/*function sum(firstnumber,secondnumber,thirdnumber){
+    let fourthnumber = firstnumber + secondnumber + thirdnumber
+
+    console.log(`the sum of all numbers is ${fourthnumber}`)
+}
+sum(32,29,67)*/
+
+/*function areaofcircle(radius){
+    let pi=3.146
+    let area=pi*radius*radius
+    console.log(`the area of circle is ${area}`)
+
+
+}
+areaofcircle(163)*/
+
+//array
+/*let fruits=['mango','banana','orange','apple']
+let firstfruit= fruits[0]
+fruits[3]='orange';
+
+console.log(firstfruit,fruits)*/
+
+//object
+/*let car={
+    make:'model',
+    model:'new model',
+    year:2018,
+    hybrid:'false',
+    colors:['black','blue','silver'],
+    methods:['drive','stop'],
+
+    hello:function(){
+        console.log("hello")
+    },
+     stop(){
+        console.log("stop")
+     }
+}
+console.log(car.make)
+console.log(car.colors[0])
+car.hello
+car.stop*/
+
+/*function calculateTotal(subTotal,tax){
+    let total=subTotal + tax
+    console.log(`${total}`)
+}
+calculationTotal(22000,3200)
+
+    let order1=2500
+    let order2=900
+    let order3=780
+    console.log(`${order1} + ${order2} + ${order3}`)*/
+
+    /*let firstname= "Trupti"
+    let lastname= "Hirani"
+    let age=18
+    if(age >=18){
+        console.log('you are an adult')
+    }
+    else{
+        console.log('you are a child')
+
+    }*/
+
+   /* let firstname ='hirani'
+    
+    let age=18
+    if(age==18 && firstname =="hirani"){
+        console.log(`you have passed the test`)
+    }
+else{
+    console.log(`you have failed the test`)
+}*/
+
+/*let person1={
+    firstname :'Trupti',
+    age:19,
+    status:'resident'
+}; 
+let person2 ={
+    firstname: 'Hirani',
+    age:14,
+    status:'tourist'
+};
+if(person2.age>=18 && person2.status ==="resident"){
+    console.log(`cast a vote`)
+}
+else{
+    console.log(`not eligible`)
+}
+*/
+/*let dice = 6
+switch(dice){
+    case 1:
+        console.log('you got one')
+        break;
+        case 2:
+            console.log('you got two')
+            break;
+            case 3:
+                console.log('you got three')
+                break;
+                case 4:
+                    console.log('you got four')
+                    break;
+                    case 5:
+                        console.log('you got five')
+                        break;
+                        case 6:
+                            console.log('you got six')
+
+}*/
+
+//loops
+
+for(let i=0; i<=5;i++){
+    console.log(`the value of the numbers are` +" "+ i)
+
+}
+let person={
+    fname:'Trupti',
+    lname:'Hirani',
+    age:18
+    
+
+}
+
+
+    
+
+
