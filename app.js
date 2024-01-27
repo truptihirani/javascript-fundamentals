e567526a53964da64f7705d2f4d329ab189075e3
truptihirani <hiranitrupti99@gmail.com>
2024-01-27 11:33:10 +0300

    new codes added

--- app.js
+++ app.js
@@ -169,7 +169,195 @@ let person={
 
 }*/
 
+/*let person1={
+    fname:'Trupti',
+    age:16,
+    Status:'tourist'
+};
+let person2={
+    fname:'Hirani',
+    age:32,
+    status:'resident'
+};
+if(person2.age>=18 && person2.status === "resident"){
+    console.log(`cast a vote`)
+}
+else{
+    console,log(`not eligible`)
 
+}*/
+
+/*let company={
+    brandname:'adidas',
+    year:1990,
+    location:'UK',
+    fname:'Trupti',
+    lname:'Hirani',
+
+    hello:function(firstname, lastname){
+        console.log(`good morning ${firstname}  ${lastname}`)
+    }
+
+}
+console.log(company.hello('tupti','hirani'))*/
+
+//callback function
+/*function one(){
+    console.log(`how are you doing`)
+}
+//higher order function
+    function two(names,cb){
+        
+        console.log(`how are you Mr.${names}`)
+        cb()
+    }
+two("Hirani",one)*/
+
+//array methods( map,filter, reduce, foreach)
+//foreach(does not return a new array,accepts a callback function,anonymously)
+/*let employee=[
+    {fname: 'hirani',age:18, salary:5000, school:'ics'},
+    {fname: 'trupti',age:25, salary:2500, school:'maseno'},
+    {fname: 'khushi',age:32, salary:9000, school:'moi'},
+    {fname: 'joe',age:17, salary:10000, school:'kabarak'}
+  
+]
+//callback function
+function names(eachname){
+    console.log(eachname.fname.toUpperCase())
+}
+//high order function
+let all=employee.forEach(names)*/
+
+/*let employee=[
+    {fname:'Hirani', age:24,school:'ics',gender:'female'},
+    {fname:'Trupti',age:32,school:'kabarak',gender:'female'},
+    {fname:'Joe',age:23,school:'moi',gender:'male'},
+    {fname:'Mark',age:18,school:'maseno',gender:'male'},
+    {fname:'John',age:16,school:'ics',gender:'male'}
+]
+  function names(eachname){
+    console.log(eachname.fname)
+  }
+let all=employee.forEach(names)*/
+
+//map(returns a new array,does not change the size of the original)
+/*const school=[
+    {location:'eldoret',population:500,schoolName:'ics'},
+    {location:'nairobi',population:1500,schoolName:'maseno'},
+    {location:'nakuru',population:5100,schoolName:'kabarak'},
+    {location:'kakamega',population:1000,schoolName:'moi'},
+]
+//callback function
+function names(eachname){
+    console.log(eachname.location)
+    return location
+}
+//high order function
+let all=school.forEach(names)*/
+
+//for loop
+/*for (let i=1; i<=5;i++){
+    console.log(i);
+    for (let j=1; j<=3;j++)
+    console.log("inner loop + j")
+}*/
+/*let day=7
+switch (day) {
+    case 0:
+      day = "Sunday";
+      console.log(`sunday`)
+      break;
+    case 1:
+      day = "Monday";
+      console.log(`monday`)
+      break;
+    case 2:
+       day = "Tuesday";
+       console.log(`tuesday`)
+      break;
+    case 3:
+      day = "Wednesday";
+      console.log(`wednesday`)
+      break;
+    case 4:
+      day = "Thursday";
+      console.log(`thursday`)
+      break;
+    case 5:
+      day = "Friday";
+      console.log(`friday`)
+      break;
+    case 6:
+      day = "Saturday";
+      console.log(`saturday`)
+      default:
+        console.log(`it is weekend!!!`)
+  }*/
+
+  /*function calculateTotal(subTotal,tax){
+  //let total= subtotal+tax;
+ //return total;
+  return subTotal+tax
+};
+console.log(calculateTotal(22000,320))
+let order1=calculateTotal(2100,100);
+let order2=calculateTotal(5000,200);
+let order3=calculateTotal(6000,500);
+
+console.log(order1,order2,order3)*/
+
+//filter(returns based on the condition)
+//returns a new array and can manipulate the size of the new array unlike map
+/*const person=[
+    {location:'eldoret',population:500,schoolName:'ics'},
+    {location:'nairobi',population:1500,schoolName:'maseno'},
+    {location:'nakuru',population:5100,schoolName:'kabarak'},
+    {location:'kakamega',population:1000,schoolName:'ics'},
+]
+let eachperson= person.filter(function(people){
+    if(people.schoolName==="maseno"){
+        return people
     
+    }    
+})
+console.log(eachperson)*/
+
+//find method(is good in returning unique values)
+//incase of multiple values,the first value is returned
+
+const person=[
+    {fname: 'hirani',age:18, salary:5000, school:'ics',id:1},
+    {fname: 'trupti',age:25, salary:2500, school:'maseno',id:5},
+    {fname: 'khushi',age:32, salary:9000, school:'moi',id:5},
+    {fname: 'joe',age:17, salary:10000, school:'kabarak',id:4}
+]
+
+    let eachperson= person.find(function(people){
+        if(people.id===5){
+            return people
+        
+        }    
+    })
+    console.log(eachperson)
+
+    //assignment : reduce array method(single value,array,object)
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
 
 
